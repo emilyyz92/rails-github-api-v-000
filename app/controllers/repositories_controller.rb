6e615f4a007cc786b8e5ad6b8d2389012265599a
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-21 22:02:10 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,15 @@
 class RepositoriesController < ApplicationController
 
   def index
+    #get the authenticated user: https://developer.github.com/v3/users/#get-the-authenticated-user
+    resp_login = Faraday.get('https://api.github.com/user') do |req|
+      req.params['client_id'] = CLIENT_ID
+      req.params['client_secret'] = CLIENT_SECRET
+      req.params[access_token] = session[:token]
+    end
+    @login = JSON.parse(resp_login)['login']
+    repo_url = JSON.parse(resp_login)['repos_url']
+    resp_repo = Faraday.get
   end
 
   def create
