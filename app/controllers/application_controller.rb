6806eb6ea2ce77cb6090bca6acbe360a39f93f9e
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-21 21:37:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_action :authenticate_user
+
+  CLIENT_ID = ENV['GITHUB_CLIENT_ID']
+  CLIENT_SECRET = ENV['GITHUB_CLIENT_SECRET']
 
   private
 
   def authenticate_user
+    href = "https://github.com/login/oauth/authorize?scope=user&client_id=#{CLIENT_ID}"
+    redirect_uri = CGI.escape('http://localhost:3000/auth')
+    redirect_to href unless logged_in?
   end
 
   def logged_in?
+    !!session[:token]
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,14 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
+    session_code = params['rack.request.query_hash']['code']
+    resp = Faraday.post('https://github.com/login/oauth/access_token') do |req|
+      req.body = "{client_id: #{CLIENT_ID},
+                   client_secret: #{CLIENT_SECRET},
+                   code: #{session_code},
+                   accept: :json}"
+     end
+     access_token = JSON.parse(resp)['access_token']
+     session[:token] = access_token
   end
 end
